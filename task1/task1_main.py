f4a94ed6034b1f16589c7bde6ab625c265bcfb40
Pramod Goyal <goyalpramod1729@gmail.com>
2022-07-08 22:10:25 +0530

    Removed global variable count and used enumerate to follow pythonic paradigm. Added exception handling for cases when the data cannot be retrieved. Added a while loop for job so it runs indefinitely.

--- task1/task1_main.py
+++ task1/task1_main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pandas_datareader._utils import RemoteDataError
 import time
 import schedule
 from alpha_vantage.timeseries import TimeSeries
@@ -7,7 +8,6 @@ import uuid
 
 def job():
     API_key = 'G24DSVXPY2GVBJ3M' #API key can be obtained from alpha vantage
-    count = 0
     df = pd.DataFrame()
     frames = []
     stock_name = ["Tesla Inc", "Apple Inc", "Amazon.com, Inc.", "Microsoft Corporation", "META", "Google", "Nvidia"
@@ -18,12 +18,23 @@ def job():
     def get_data(data):
         frames.append(data)
 
-    ts = TimeSeries(API_key, output_format='pandas') #retrieve the data from alpha vantage
+
+    try:
+        ts = TimeSeries(API_key, output_format='pandas') #Connects us to the alpha vantage API
+    except:
+        print("Unable to connect to alpha vantage")
+    
     with open("ticker_symbols.txt","r") as f:
         tickers= [(ticker.strip()).split() for ticker in f]
 
-    for ticker in tickers:
-        data, meta = ts.get_daily(ticker)
+
+    for count,ticker in enumerate(tickers):
+        try:
+            data, meta = ts.get_daily(ticker) #Retrieves the daily data from the API
+
+        except RemoteDataError:
+            print('No data found for {t}'.format(t=ticker))
+
         print(ticker)
         UUID = uuid.uuid1()
         data.insert(0,'ID', UUID)
@@ -38,9 +49,10 @@ def job():
     df = df.iloc[:, :-1] # drop last column 
     df.insert(loc = 0,column='Stock',value=tickers)
 
-    
+    # engine = create_engine('postgresql://postgres:admin@localhost:5432/stockdata')
     engine = create_engine('sqlite:///stockdata.db', echo=True) #creates a db file in the same directory
     df.to_sql('stock_data_new', engine)
 
-schedule.every().day.at("10:30:00").do(job) #schedules a job that runs daily at 10:30 AM
+while True:
+    schedule.every().day.at("10:30:00").do(job) #schedules a job that runs daily at 10:30 AM
 
